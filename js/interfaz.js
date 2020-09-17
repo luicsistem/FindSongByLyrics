428fe7740e11990ab615218fee969e7949d4caa4
luis <luic.sistem@gmail.com>
2020-09-17 07:51:03 -0500

    add selectrs

---
+++ js/interfaz.js
@@ -0,0 +1,7 @@
+// selectores 
+
+export  const formBuscar = document.querySelector('#formulario-buscar'),
+              divBuscar = document.querySelector('#buscar'),
+              message = document.querySelector('#mensajes'),
+              result = document.querySelector('#resultado'),
+              headingTitle = document.querySelector('.letra-resultado h2');
